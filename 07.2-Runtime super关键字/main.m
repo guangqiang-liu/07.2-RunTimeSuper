8b63a111ae144f3ae56ba7a96674d9c677999632
刘光强 <1126756952@qq.com>
2020-02-08 14:02:19 +0800

    Initial Commit

---
+++ 07.2-Runtime super关键字/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  07.2-Runtime super关键字
+//
+//  Created by 刘光强 on 2020/2/8.
+//  Copyright © 2020 guangqiang.liu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
